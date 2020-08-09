69ceeafc84554e9005fe1fdfd4b2beb84eab4d2a
Jozef Fulop <jozef.fulop@innovatrics.com>
2020-08-09 20:16:38 +0200

    Update

--- mkdocs/spec/image_spec.rb
+++ mkdocs/spec/image_spec.rb
@@ -7,7 +7,7 @@ describe 'Dockerfile' do
     it { should have_entrypoint ['mkdocs'] }
     it { should have_cmd        ['--help'] }
     it { should have_expose     '8000' }
-    it { should have_user       'nobody' }
+    it { should have_user       'dev' }
 
     describe docker_run(described_image) do
       its(:stdout) { should eq '' }
